27e61ee6f9b0480f856c30741c1ee3801e48379d
Ozan A. <ozan.ayik@goldoak.ch>
2025-02-28 11:52:35 +0100

    v0.5

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t goldoak-module-greenbone:v0.4 .
-docker tag goldoak-module-greenbone:v0.4 ghcr.io/oa-goldoak/goldoak-module-greenbone:v0.4
-docker push ghcr.io/oa-goldoak/goldoak-module-greenbone:v0.4
+docker build -t goldoak-module-greenbone:v0.5 .
+docker tag goldoak-module-greenbone:v0.5 ghcr.io/oa-goldoak/goldoak-module-greenbone:v0.5
+docker push ghcr.io/oa-goldoak/goldoak-module-greenbone:v0.5
